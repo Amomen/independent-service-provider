3c2c5d691cce24e0818691222337c82e82d94948
unknown <amomen424@gmail.com>
2022-04-19 00:44:31 +0600

    md edited

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import AboutMe from "./Components/AboutMe/AboutMe";
-import Blog from "./Components/Blog/Blog";
-import CheckOut from "./Components/CheckOut/CheckOut";
-import Footer from "./Components/Footer/Footer";
-import Header from "./Components/Header/Header";
-import Home from "./Components/Home/Home";
-import ServiceSinglePage from "./Components/Home/Slider/ServiceSinglePage/ServiceSinglePage";
-import Login from "./Components/Login/Login/Login";
-import Register from "./Components/Login/Register/Register";
-import NotFound from "./Components/NotFound/NotFound";
-import RequireAuth from "./Components/RequireAuth/RequireAuth";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="">
-      <Header></Header>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/blog" element={<Blog />}></Route>
-        <Route
-          path="/service/:serviceId"
-          element={
-            <RequireAuth>
-              <ServiceSinglePage></ServiceSinglePage>
-            </RequireAuth>
-          }
-        ></Route>
-        <Route path="/aboutme" element={<AboutMe />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
-      <Footer></Footer>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
